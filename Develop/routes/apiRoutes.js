720bd23898236f508f5840bfa42815794b305515
Grant Smith <grantnsmith@gmail.com>
2020-05-13 18:38:00 -0700

    Created function to add unique ID to each new note. (#5)

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,15 +4,20 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  
+// Get route
 app.get("/api/notes", function(req, res) {
     res.json(noteArray);
   });
 
+// Post route
 app.post("/api/notes", function(req, res) {
   var newNote = req.body;
   noteArray.push(newNote)
-  
+
+  // Add ID property to each new note
+  uniqueID(noteArray);
+
+  // Write new JSON file with update array
   fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(noteArray), (err) => {
     if (err) {
       console.log("file not updated");
@@ -24,6 +29,14 @@ app.post("/api/notes", function(req, res) {
   
   });
 
-
 };
+
+// Add ID to new note
+var uniqueID = function(arr) {
+  for (i = 0; i < arr.length; i++) {
+    arr[i].id = i + 1;
+  }
+}
+
+
   
