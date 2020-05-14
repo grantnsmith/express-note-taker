917899e08e22068c2d2a1047c13ccd9af01e2151
Grant Smith <grantnsmith@gmail.com>
2020-05-13 17:11:26 -0700

    Added post route - db.json is updated and new note is returned.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,29 @@
-var notes = require("../db/db.json");
+var noteArray = require("../db/db.json");
+var fs = require("fs");
+var path = require("path");
 
 module.exports = function(app) {
 
   
-    app.get("/api/notes", function(req, res) {
-      res.json(notes);
-    });
+app.get("/api/notes", function(req, res) {
+    res.json(noteArray);
+  });
+
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  noteArray.push(newNote)
   
-  };
+  fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(noteArray), (err) => {
+    if (err) {
+      console.log("file not updated");
+    } else {
+      console.log("file updated");
+    }
+  })
+  res.json(newNote);
+  
+  });
+
+
+};
   
