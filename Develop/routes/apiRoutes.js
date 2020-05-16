d99625eedecdff4f4c107c9da5661d3c5db6d542
Grant Smith <grantnsmith@gmail.com>
2020-05-15 18:43:11 -0700

    Final touches, added comments, deleted HW instructions file.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,12 +4,12 @@ var path = require("path");
 
 module.exports = function(app) {
 
-// Get route
+// Get route - reads from db.json
 app.get("/api/notes", function(req, res) {
     res.json(noteArray);
   });
 
-// Post route
+// Post route - post to db.json
 app.post("/api/notes", function(req, res) {
   var newNote = req.body;
   noteArray.push(newNote);
@@ -17,7 +17,7 @@ app.post("/api/notes", function(req, res) {
 // Add ID property to each new note
   uniqueID(noteArray);
 
-// Write new JSON file with update array
+// Write new JSON file with updated array
   fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(noteArray), (err) => {
     if (err) {
       console.log("file not updated");
@@ -29,15 +29,17 @@ app.post("/api/notes", function(req, res) {
   
   });
 
-  // Add ID to new note
+  // Function to add ID to new note
 var uniqueID = function(arr) {
   for (i = 0; i < arr.length; i++) {
     arr[i].id = i + 1;
   }
 }
 
-// Delete route
+// Delete route - deletes from db.json
 app.delete("/api/notes/:id", function(req, res) {
+  
+// Looking for note with corresponding ID  
   var noteID = req.params.id;
     for (i = 0; i < noteArray.length; i++) {
       if (noteID == noteArray[i].id) {
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,11 +2,12 @@ var path = require("path");
 
 module.exports = function(app) {
    
-  
+  // Notes page
     app.get("/notes", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
-  
+
+  // Home page
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
