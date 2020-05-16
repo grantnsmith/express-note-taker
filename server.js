5d4a675d5f55ad4f5635f304cc94c2f4afd23109
Grant Smith <grantnsmith@gmail.com>
2020-05-15 20:01:20 -0700

    Rearranged file structure to make it work with Heroku.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
